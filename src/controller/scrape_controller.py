afa610628fefb32e679f885668699494cdc98df9
tawfikhammad <tawfikhammad@icloud.com>
2025-02-02 03:59:11 +0200

    control scraping processes

--- src/controller/scrape_controller.py
+++ src/controller/scrape_controller.py
@@ -1,5 +1,4 @@
 import logging
-from typing import List
 from config import settings
 from scraper import PDFScraper, GetFile
 from helper import FileValidator
@@ -9,32 +8,39 @@ from database import VectorDBManager
 class ScrapeController:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self.existing_docs = set()
+        self.vdb_manager = VectorDBManager()
+        self.should_scrape = self.vdb_manager.is_collection_empty()
+        self.scraper = PDFScraper(settings.TARGET_WEBSITE)
 
     async def store_into_vdb(self):
-        categories = PDFScraper.get_main_categories()
+        if not self.should_scrape:
+            print("Database already populated. Skipping scraping.")
+            return
+
+        categories = self.scraper.get_main_categories()
         
         for category, category_url in categories:
-            subcategories = PDFScraper.get_subcategory_links(category_url)
+            subcategories = self.scraper.get_subcategory_links(category_url)
             stored_count = 0
             
             for subcategory, sub_url, file_title in subcategories:
                 if stored_count >= settings.MAX_PDFS_PER_CATEGORY:
                     break
 
-                file_url, file_type, file_size  = PDFScraper.find_pdf_link(sub_url)
+                file_url, file_type, file_size  = self.scraper.find_pdf_link(sub_url)
                 if not file_url and file_type and file_size:
                     continue
                 
-                isvalid, message = FileValidator.isvalid(file_type, file_size)
+                isvalid, message = await FileValidator.isvalid(file_type, file_size)
                 if not isvalid:
                     self.logger.error(f"Invalid file: {file_title} - {message}")
                     continue
 
-                file_path = await GetFile.file_path(file_url, file_title, category, subcategory)
+                file_path = await GetFile().file_path(file_url, category, subcategory)
 
                 # Process and store
-                document = PDFProcessor().process_pdf(file_path, file_title, sub_url, category, subcategory)
+                processor = PDFProcessor()
+                document = await processor.process_pdf(file_path, file_title, sub_url, category, subcategory)
                 if document:
                     VectorDBManager().store_document(document)
                     stored_count += 1
