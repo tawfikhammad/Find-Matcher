2356de8ded78fc46b65f8b5d6b1aabf4c4846cc0
tawfikhammad <tawfikhammad@icloud.com>
2025-02-02 03:58:46 +0200

    write sime app enums

---
+++ src/helper/enums/Enums.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class ValidationEnums(Enum):
+
+    NOT_ALLOWED_FILE_TYPE = "Unsupported File Type"
+    INVALID_FILE_SIZE = "File Size Exceeds Allowed Limit"
+    VALID_FILE = "File Validation Successful"
+    INVALID_FILE = "File Validation Unsuccessful"
+
+class ScanEnums(Enum):
+
+    NATIVE_PDF = "Native PDF"
+    SCANNED_PDF = "Scanned PDF" 
